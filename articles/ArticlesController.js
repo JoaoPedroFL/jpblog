b367ca98e18eb294fee4c160493cfdd95bc14584
João Pedro <estudante2025@gmail.com>
2022-04-19 23:50:06 -0300

    Rota novo artigo criada

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router()
 const Category = require("../categories/Category")
+const Article = require("./Article")
+const slugify = require("slugify")
 
 router.get("/articles", (req, res) => {
     res.send("Rota de Articles.")
@@ -9,6 +11,19 @@ router.get("/articles", (req, res) => {
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", {categories: categories})
+    })
+})
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.Category
+
+    Article.create({
+        title: title,
+        slugify: slugify(title),
+        body: body,
+        categoryId: category
     })
 })
 
